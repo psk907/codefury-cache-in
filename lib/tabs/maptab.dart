b29b30804475b3191a4b48ed27a5f0a3a1839a30
reeshaa <shenoyreesha@gmail.com>
2020-11-07 11:32:25 +0530

    bottom nav basic bp

---
+++ lib/tabs/maptab.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MapTab extends StatefulWidget {
+  @override
+  _MapTabState createState() => _MapTabState();
+}
+
+class _MapTabState extends State<MapTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("map"));
+  }
+}
